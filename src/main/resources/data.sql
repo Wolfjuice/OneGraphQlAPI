b3f650bc0a8fb1aae6f05bb390d700e5eca1d7e1
Wolfjuice <45112960+Wolfjuice@users.noreply.github.com>
2021-08-27 11:53:33 -0400

    Add files via upload

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO location (id, name, address) VALUES (1, 'ATL Airport','Atlanta airport location');
+INSERT INTO location (id, name, address) VALUES (2, 'MIA Airport','Miami airport location');
+INSERT INTO location (id, name, address) VALUES (3, 'LAX Airport','Los Angeles airport location');
+INSERT INTO location (id, name, address) VALUES (4, 'PHX Airport','Phoenix airport location');
+INSERT INTO location (id, name, address) VALUES (5, 'ORD Airport','Chicago airport location');
+INSERT INTO location (id, name, address) VALUES (6, 'One','Two');
+INSERT INTO location (id, name, address) VALUES (7, 'Three','Four');
+INSERT INTO location (id, name, address) VALUES (8, 'Five','Six');
+INSERT INTO location (id, name, address) VALUES (9, 'Seven','Eight');
+INSERT INTO location (id, name, address) VALUES (10, 'Nine','Ten');
---
+++ target/classes/data.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO location (id, name, address) VALUES (1, 'ATL Airport','Atlanta airport location');
+INSERT INTO location (id, name, address) VALUES (2, 'MIA Airport','Miami airport location');
+INSERT INTO location (id, name, address) VALUES (3, 'LAX Airport','Los Angeles airport location');
+INSERT INTO location (id, name, address) VALUES (4, 'PHX Airport','Phoenix airport location');
+INSERT INTO location (id, name, address) VALUES (5, 'ORD Airport','Chicago airport location');
+INSERT INTO location (id, name, address) VALUES (6, 'Jose','Mexico');
+INSERT INTO location (id, name, address) VALUES (7, 'Hindutva','Bharat');
+INSERT INTO location (id, name, address) VALUES (8, 'Morgue','Caffeinistan');
+INSERT INTO location (id, name, address) VALUES (9, 'Cornheel','Isntreal');
+INSERT INTO location (id, name, address) VALUES (10, 'Myloo','Karela');
